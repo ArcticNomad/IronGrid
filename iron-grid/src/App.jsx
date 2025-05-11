0a1f2742b76e8b2b3938565f1731579ee5378627
Ahmad <153628471+ArcticNomad@users.noreply.github.com>
2025-05-11 17:16:12 +0500

    ok

--- iron-grid/src/App.jsx
+++ iron-grid/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className="bg-red-500 text-white p-4">
-  If you see a red box, Tailwind is working ✅
-</div>
-
+    <section className='Hero'>
+      <Hero />
+    </section>
+    
     </>
   )
 }
